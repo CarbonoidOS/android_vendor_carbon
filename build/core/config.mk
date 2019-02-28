b734d97da636b1e06bc4b9512a37eac323936fd2
Rashed Abdel-Tawab <rashed@linux.com>
2019-02-28 04:40:02 +0000

    carbon: Ignore neverallows
    Change-Id: I35a39ec73c0047f93124f72249a6fa936fe0875a

--- build/core/config.mk
+++ build/core/config.mk
@@ -25,3 +25,8 @@ include $(TOPDIR)vendor/carbon/build/core/mtk_target.mk
 include $(TOPDIR)vendor/carbon/build/core/qcom_target.mk
 
 BUILD_RRO_SYSTEM_PACKAGE := $(TOPDIR)vendor/carbon/build/core/system_rro.mk
+
+# We modify several neverallows, so let the build proceed
+ifneq ($(TARGET_BUILD_VARIANT),user)
+SELINUX_IGNORE_NEVERALLOWS := true
+endif
